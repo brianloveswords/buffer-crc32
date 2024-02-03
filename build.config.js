5807b367622b19e5e186fb30716184c4a90a55dd
Igor Savin <iselwin@gmail.com>
2024-02-03 02:01:16 +0200

    Make arethetypeswrong happy

--- build.config.js
+++ build.config.js
@@ -1,6 +1,7 @@
 export default {
-    entries: ["./index.js"],
-    rollup: {
-        emitCJS: true,
-    },
+  entries: ['./index.js'],
+  rollup: {
+    emitCJS: true,
+  },
+  failOnWarn: false,
 };
